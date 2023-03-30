e3409f04405f54059e442f99a0415c145b5ef861
margoshkari <68592214+margoshkari@users.noreply.github.com>
2023-03-30 18:33:57 +0300

    remove

--- autoservice/src/components/Warehouse.js
+++ autoservice/src/components/Warehouse.js
@@ -3,16 +3,24 @@ import styles from '../styles/WarehouseModule.module.css'
 
 function Warehouse(){
     const [data, SetData] = useState([{id: 1, name: "Sklad", address: "Address"}]);
+
+    //УДАЛЕНИЕ
+    function RemoveData(id){
+        const newData = data.filter(item => item.id !== id);
+        SetData(newData);
+    }
     return (
-        <div>
-            {data.map((item) => {
-                 return (
-                    <div key={item.id} className={styles.card}>
-                        <h2>{item.name}</h2>
-                        <h3>{item.address}</h3>
-                    </div>
-                );
-            })}
+        <div className={styles.cards}>
+            {!data ? (<div>No warehouse found</div>) : 
+                data.map((item) => {
+                    return (
+                        <div key={item.id} className={styles.card}>
+                            <h2>{item.name}</h2>
+                            <h3>{item.address}</h3>
+                            <button className={styles.removeBtn} onClick={() => RemoveData(item.id)}>Remove</button>
+                        </div>
+                    );
+                })}
         </div>
     );
 }
